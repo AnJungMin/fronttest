73e8976dacbdae05c3a448808257026a36315cd3
AnJungMin <dkswjdals4027@gmail.com>
2025-05-28 23:49:04 +0900

    add two dot parents folder

--- src/pages/DiagnosisPage.jsx
+++ src/pages/DiagnosisPage.jsx
@@ -1,7 +1,7 @@
 import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import Webcam from "react-webcam";
-import ResultCard from "./ResultCard";
+import ResultCard from "../components/ResultCard";
 
 export default function DiagnosisPage() {
   const [image, setImage] = useState(null);
